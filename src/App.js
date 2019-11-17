0f926e8181ad509a7ea095698fef2328adc7575c
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-11-17 15:46:04 -0800

    using hooks, set us eslint rules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 /** @format */
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  // return React.createElement('div', {}, [
-  //   React.createElement('h1', {}, 'Adopt Me!'),
-  //   React.createElement(Pet, { name: 'Penny', animal: 'Dog', breed: 'Feist' }),
-  //   React.createElement(Pet, { name: 'Ralph', animal: 'Dog', breed: 'Feist' }),
-  //   React.createElement(Pet, { name: 'Karina', animal: 'Cat', breed: 'Stray' }),
-  // ]);
   return (
     <div>
       <h1 id="boo">Adopt Me!</h1>
-      <Pet name="Penny" animal="Dog" breed="Feist" />
-      <Pet name="Ralph" animal="Dog" breed="Feist" />
-      <Pet name="Karina" animal="Cat" breed="Stray" />
+      <SearchParams />
     </div>
   );
 };
