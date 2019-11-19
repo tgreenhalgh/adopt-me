705beefd379cb15e35897c05494d0d0571c61c81
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-11-19 15:01:23 -0800

    clarifies pet API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -15,9 +15,9 @@ const SearchParams = () => {
     setBreeds([]);
     setBreed('');
 
-    // pet. retrieves info from API
-    pet.breeds(animal).then(({ breeds }) => {
-      const breedStrings = breeds.map(({ name }) => name);
+    // pet is retrieved from API
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error); // equivalent to e => console.error(e);
   }, [animal, setBreed, setBreeds]);
