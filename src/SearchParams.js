270a5ff2a7b5684fd55b0e27847d3b738a68cf00
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-11-18 18:20:17 -0800

    useEffect notes

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,31 @@
 /** @format */
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropDown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+
+  // NOTE: useEffect happens after the render is done
+  useEffect(() => {
+    // clear out the lists
+    setBreeds([]);
+    setBreed('');
+
+    // pet. retrieves info from API
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error); // equivalent to e => console.error(e);
+  }, [animal, setBreed, setBreeds]);
+  /* ^
+     the array is a list of what to monitor changes in
+     so when `animal` changes, useEffect fires
+     }, []);  <- if you do empty array, runs first time, never again
+  */
 
   return (
     <div className="search-params">
