858dae0b91e8ae1f43c50bcdbbdd0cc6029f004e
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-11-24 16:23:22 -0800

    starts carousel

---
+++ src/Carousel.js
@@ -0,0 +1,54 @@
+/** @format */
+import React from 'react';
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0,
+  };
+
+  // React method take takes props and returns state
+  static getDerivedStateFromProps({ media }) {
+    let photos = ['http://placecorgi.com/600/600'];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    // return the object to be merged into state
+    return { photos };
+  }
+
+  // NOTE: an arrow function, so `this` is correct
+  // in general, make all event listeners arrow functions
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? 'active' : ''}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
